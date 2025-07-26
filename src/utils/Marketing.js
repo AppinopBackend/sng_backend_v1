0195ff46934c7779787c104ac1ab30481a91a4a5
shailesh <shailesh@appinop.com>
2025-07-26 12:05:51 +0530

    fix changes

--- src/utils/Marketing.js
+++ src/utils/Marketing.js
@@ -9,7 +9,7 @@ module.exports = {
       const transporter = nodemailer.createTransport({
         host: process.env.MAILER_HOST,
         port: process.env.MAILER_PORT,
-        secure: false,
+        secure: true,
         auth: {
           user: process.env.MAILER_USER,
           pass: process.env.MAILER_PASS,
